33c4b1b033ae35c1b23086244c4b082fb0f97231
Stefel Smith <stefelsmith@gmail.com>
2024-02-24 01:49:55 -0500

    ADDED import from add and remove action history and export to inventory

--- src/routes/shoppinglist-router.js
+++ src/routes/shoppinglist-router.js
@@ -2,12 +2,19 @@ import express from "express";
 import {
   createNewShoppingList,
   getUserShoppingLists,
-  addShoppingListItems,
+  addShoppingListItem,
   getUserShoppingList,
+  deleteItemFromList,
+  addShoppingListItems
 } from "../services/shoppinglist-service.js";
 import ShoppingList from "../models/ShoppingList.js";
 import { isMongoDuplicate } from "../util.js";
 import { Types } from "mongoose";
+import Inventory from "../models/Inventory.js";
+import FoodItem from "../models/FoodItem.js";
+import InventoryAddAction from "../models/InventoryAddAction.js";
+import InventoryRemoveAction from "../models/InventoryRemoveAction.js";
+import ShoppingListItem from "../models/ShoppingListItem.js";
 
 const shoppingListRouter = express.Router();
 
@@ -45,12 +52,12 @@ shoppingListRouter.get("/allitems", async (req, res, next) => {
     const { userId, title } = req.query;
     const shoppinglist = await getUserShoppingList(userId, title);
 
-    if(shoppinglist.length > 0) {
+    if (shoppinglist.length > 0) {
       res.json(shoppinglist[0].shoppingListItems).end();
     } else {
       res.send(shoppinglist) // this is a an empy list
     }
-     
+
   } catch (err) {
     next(err);
   }
@@ -61,12 +68,12 @@ shoppingListRouter.get("/import", async (req, res, next) => {
     const { userId, title } = req.query;
     const shoppinglist = await getUserShoppingList(userId, title);
 
-    if(shoppinglist.length > 0) {
+    if (shoppinglist.length > 0) {
       res.json(shoppinglist[0].shoppingListItems).end();
     } else {
       res.send(shoppinglist) // this is a an empy list
     }
-     
+
   } catch (err) {
     next(err);
   }
@@ -76,33 +83,21 @@ shoppingListRouter.get("/import", async (req, res, next) => {
 shoppingListRouter.put("/additem", async (req, res, next) => {
   try {
     const { userId, title, foodItem, foodAmount } = req.body;
-    const items = await addShoppingListItems(userId, title, foodItem, foodAmount)
+    const items = await addShoppingListItem(userId, title, foodItem, foodAmount)
     res.json(items).end()
   } catch (err) {
     next(err);
   }
 });
 
-shoppingListRouter.delete("/delete",  async (req, res, next) => {
+shoppingListRouter.delete("/delete", async (req, res, next) => {
   const { userId, title, itemId } = req.body;
-  
+
   try {
     // Find the shopping list based on userId and title
     const shoppingList = await ShoppingList.findOne({ ownerId: new Types.ObjectId(userId), title });
 
-    if (!shoppingList) {
-      return res.status(404).json({ message: 'Shopping list not found' });
-    }
-
-    // Find the index of the item to delete
-    const index = shoppingList.shoppingListItems.findIndex(item => item._id.toString() === itemId);
-
-    if (index === -1) {
-      return res.status(404).json({ message: 'Item not found in shopping list' });
-    }
-
-    // Remove the item from the shoppingListItems array
-    shoppingList.shoppingListItems.splice(index, 1);
+    deleteItemsFromList(shoppingList);
 
     // Save the updated shopping list
     await shoppingList.save();
@@ -113,5 +108,77 @@ shoppingListRouter.delete("/delete",  async (req, res, next) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 })
+
+// Route to import items from user history into a shopping list
+shoppingListRouter.post("/import", async (req, res) => {
+  const { userId, shoppingListName } = req.body;
+
+  try {
+    // Find all add actions  and remove actions for the user
+    const addActions = await InventoryAddAction.find({ ownerId: userId });
+    const removeActions = await InventoryRemoveAction.find({ ownerId: userId });
+
+    console.log(addActions.length)
+    if (addActions.length < 1 || removeActions.length < 1) {
+      return res.status(400).json({ message: 'No User History, add items manualy' });
+    }
+
+    // Initialize a map to store item scores based on their names
+    const itemScores = new Map();
+
+
+    // Compute scores for items based on add and remove actions
+    addActions.forEach((addAction) => {
+      const itemName = addAction.foodItem.name;
+      const score = itemScores.get(itemName) || 0;
+      itemScores.set(itemName, score + 1);
+    });
+
+    removeActions.forEach((removeAction) => {
+      const itemName = removeAction.foodItem.name;
+      const score = itemScores.get(itemName) || 0;
+      itemScores.set(itemName, score + 2);
+    });
+
+
+    // Filter items based on score threshold and add them to the shopping list
+    const itemsToAdd = [];
+    itemScores.forEach((score, itemName) => {
+      if (score >= 3) { // Change the threshold as needed
+        // Find the most recently used quantity from addActions
+        const mostRecentAddAction = addActions
+          .filter((addAction) => addAction.foodItem.name === itemName)
+          .sort((a, b) => b.date - a.date)[0]; // Sort by date in descending order and get the first item
+        const quantity = mostRecentAddAction ? mostRecentAddAction.foodItem.quantity : 1;
+        const newItem = new ShoppingListItem({
+          title: itemName,
+          amount: quantity,
+          price: 0,
+          importance: 0,
+        })
+        itemsToAdd.push(newItem);
+      }
+    });
+
+
+    // Create a new shopping list and add the items to it
+    if (itemsToAdd.length > 0) {
+      let shoppingList = await createNewShoppingList(shoppingListName, userId);
+      shoppingList = await addShoppingListItems(userId, shoppingList.title, itemsToAdd);
+      return res.send(shoppingList).status(200);
+    } else {
+      return res.status(400).json({ message: 'Need to build a history add items manually' });
+    }
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+
+
 
 export default shoppingListRouter;
--- src/services/shoppinglist-service.js
+++ src/services/shoppinglist-service.js
@@ -1,6 +1,8 @@
 import ShoppingList from "../models/ShoppingList.js";
 import { Types } from "mongoose";
 import ShoppingListItem from "../models/ShoppingListItem.js";
+import InventoryAddAction from "../models/InventoryAddAction.js";
+import InventoryRemoveAction from "../models/InventoryRemoveAction.js";
 
 export const createNewShoppingList = async (listTitle, userId) => {
   console.log("creating new list")
@@ -44,7 +46,7 @@ export const getAllUserInventoryItems = async (userId) => {
   return allFoodItems;
 };
 
-export const addShoppingListItems = async (userId, title, foodItem, foodAmount) => {
+export const addShoppingListItem= async (userId, title, foodItem, foodAmount) => {
   const item = new ShoppingListItem();
   item.title = foodItem;
   item.amount = foodAmount;
@@ -74,3 +76,69 @@ export const addShoppingListItems = async (userId, title, foodItem, foodAmount) 
   await rlist.save();
   return rlist;
 };
+
+export const addShoppingListItems = async (userId, shoppingListName, itemsToAdd) => {
+  try {
+    // Find the shopping list by name
+    const shoppingList = await ShoppingList.findOne({ ownerId: userId, title: shoppingListName });
+
+    if (!shoppingList) {
+      return { success: false, message: 'Shopping list not found' };
+    }
+
+    // Add each item to the shopping list
+    itemsToAdd.forEach(async (item) => {
+      const title = item.title;
+      const amount = item.amount;
+      const existingItemIndex = shoppingList.shoppingListItems.findIndex(existingItem => existingItem.title === title);
+
+      if (existingItemIndex !== -1) {
+        // If the item exists, update its quantity
+        shoppingList.shoppingListItems[existingItemIndex].amount += amount;
+      } else {
+        // If the item does not exist, add it to the shopping list
+        shoppingList.shoppingListItems.push({ title, amount });
+      }
+    });
+
+
+    return await shoppingList.save();
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: 'Internal server error' };
+  }
+};
+                       
+
+export const deleteItemFromList = async (shoppingListName, itemName) => {
+   // Find the shopping list by name
+   const shoppingList = await ShoppingList.findOne({ title: shoppingListName });
+
+  if (!shoppingList) {
+    return res.status(404).json({ message: 'Shopping list not found' });
+  }
+
+  // Find the index of the item to delete
+  const index = shoppingList.shoppingListItems.findIndex(item => item.title.toString() === itemName);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Item not found in shopping list' });
+  }
+
+  // Remove the item from the shoppingListItems array
+  shoppingList.shoppingListItems.splice(index, 1);
+
+  // If the shopping list is empty, delete the shopping list
+  if (shoppingList.shoppingListItems.length === 0) {
+    await ShoppingList.deleteOne({ _id: shoppingList._id });
+    return { status: 200, message: 'Item deleted and shopping list is now empty' };
+  }
+
+  // Save the updated shopping list
+  await shoppingList.save();
+};
+
+
+
+
+
